fe5b0fe33b9b8a6f300ce52ced645c459b3516c4
rose534 <wambuirose534@gmail.com>
2023-03-22 21:13:27 +0300

    lab

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
     def create
         user = User.create(user_params)
         if user.valid?
-          session[:user_id] = user.id # Save the user id in the session
+          session[:user_id] = user.id 
           render json: user, status: :created
         else
           render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
